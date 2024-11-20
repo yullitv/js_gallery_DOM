cf577a7e4b29717f0e6215ab74bcfca3f6424903
Yullit <yuliok03@gmail.com>
2024-11-20 20:00:58 +0200

    make build

---
+++ index.244ac6f7.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".gallery__thumb").parentElement.getAttribute("href");t.src=r});
+//# sourceMappingURL=index.244ac6f7.js.map
